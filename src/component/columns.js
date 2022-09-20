e764addb1fe44dc69699a971d17e3dfe99cd9111
unknown <kirankthota79@gmail.com>
2022-09-20 09:58:12 +0100

    first commit

---
+++ src/component/columns.js
@@ -0,0 +1,34 @@
+export const COLUMNS =[
+    {
+        Header:'Id',
+        Footer:'Id',
+        accessor: 'id'
+    },
+    {
+        Header:'First Name',
+        Footer:'First Name',
+        accessor:'first_name'
+    },
+    {
+        Header:'Last Name',
+        Footer:'Last Name',
+        accessor:'last_name'
+
+    },
+    {
+        Header:'Date of Birth',
+        Footer:'Date of Birth',
+        accessor:'Date_of_Birth'
+    },
+    {
+        Header: 'Country',
+        Footer: 'Country',
+        accessor:'country'
+    },
+    {
+        Header: 'Phone',
+        Footer: 'Phone',
+        accessor:'phone'
+    },
+
+]
